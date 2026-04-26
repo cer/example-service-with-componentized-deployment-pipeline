12f2c2f9ba0720a7275a9017b4b023e855c86369
Chris Richardson <Chris@ChrisRichardson.net>
2026-04-25 17:28:08 -0700

    Add unit test for POST /customers endpoint
    Co-authored by Claude Code

--- customer-management/customer-management-web-api/src/test/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/web/CustomerControllerTest.java
+++ customer-management/customer-management-web-api/src/test/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/web/CustomerControllerTest.java
@@ -1,6 +1,8 @@
 package io.eventuate.examples.tram.sagas.ordersandcustomers.customers.web;
 
 
+import io.eventuate.examples.common.money.Money;
+import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.domain.Customer;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.domain.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,12 +10,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Collections;
 
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +28,30 @@ public class CustomerControllerTest {
 
   @InjectMocks
   private CustomerController customerController;
+
+  @Test
+  public void shouldCreateCustomer() {
+    String name = "Fred";
+    Money creditLimit = new Money("15.00");
+    Long customerId = 42L;
+
+    Customer customer = new Customer(name, creditLimit);
+    ReflectionTestUtils.setField(customer, "id", customerId);
+
+    when(customerService.createCustomer(name, creditLimit)).thenReturn(customer);
+
+    given()
+            .standaloneSetup(customerController)
+            .contentType(JSON)
+            .body(new io.eventuate.examples.tram.sagas.ordersandcustomers.customers.api.web.CreateCustomerRequest(name, creditLimit))
+      .when()
+            .post("/customers")
+            .then()
+            .log().ifValidationFails()
+            .statusCode(HttpStatus.OK.value())
+            .contentType(JSON)
+            .body("customerId", equalTo(customerId.intValue()));
+  }
 
   @Test
   public void shouldGetCustomers() {
